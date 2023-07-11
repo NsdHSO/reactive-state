bbd7a746112618ddf06c39a9731459b8d66512d7
( Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2023-07-11 06:25:47 +0300

    chore:🌻 added idea in gitignore and another aproach to functional programming

--- index.js
+++ index.js
@@ -2,59 +2,60 @@
 
 class Button {
   constructor(name) {
-    this.name = name
-    this.button = document.createElement('button')
-    this.button.innerText = name
-    this.button.addEventListener('click', this.click.bind(this))
-    this.renderer(this.button)
+    this.name = name;
+    this.button = document.createElement('button');
+    this.button.innerText = name;
+    this.button.addEventListener('click', this.click.bind(this));
+    this.renderer(this.button);
   }
 
   click() {
-    console.log('nda')
+    console.log('nda');
   }
 
   renderer(element) {
-    console.log(element)
-    document.body.appendChild(element)
-    console.log('renderer ')
+    console.log(element);
+    document.body.appendChild(element);
+    console.log('renderer ');
   }
 }
 
 class Input {
   constructor(elementId) {
-    this.element = document.createElement('input')
-    this.element.id = elementId
-    this.value = ''
+    this.element = document.createElement('input');
+    this.element.id = elementId;
+    this.value = '';
 
     // Bind the event listener to the input element
-    this.element.addEventListener('input', this.handleInput.bind(this))
-    this.render()
+    this.element.addEventListener('input', this.handleInput.bind(this));
+    this.render();
   }
 
   handleInput(event) {
-    this.value = event.target.value
-    console.log('Input value:', this.value)
+    this.value = event.target.value;
+    console.log('Input value:', this.value);
   }
 
   getValue() {
-    return this.value
+    return this.value;
   }
 
   setValue(newValue) {
-    this.value = newValue
-    this.element.value = newValue
+    this.value = newValue;
+    this.element.value = newValue;
   }
 
   clear() {
-    this.setValue('')
+    this.setValue('');
   }
 
   render() {
-    document.body.appendChild(this.element)
+    document.body.appendChild(this.element);
   }
 }
-function wordScore(word) {
-  return [...word].filter(v => v !== 'a').join('').length
-}
 
-console.log(wordScore('Sami dsdsd dasd'))
+
+const cleanerWord = (c) => c.replace(/ +/g, '')
+const filterWord = (arr) => Array.from(arr).filter(char => char !== 'a')
+const joinedWord= (v)=> filterWord(v).join('')
+console.log(cleanerWord(joinedWord(['dasdd','dasdasd','dasdasd', 'dasdasd'])));
